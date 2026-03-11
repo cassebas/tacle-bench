209f62d77d1e8bbb85b2038272c4dc7d2740782d
Caspar Treijtel <18738369+cassebas@users.noreply.github.com>
2026-03-11 10:44:56 +0100

    Bsort: remove printing of sorted array

--- bench/kernel/bsort/bsort.c
+++ bench/kernel/bsort/bsort.c
@@ -187,12 +187,6 @@ int main( void )
   bsort_main();
   asm volatile ("csrr %0, mcycle" : "=r" (cycles2));
 
-  kprintf("bsort sorted input is : ");
-  for (int i=0; i<bsort_SIZE; i++) {
-    kprintf("%d ", bsort_Array[i]);
-  }
-  kprintf("\n");
-
   kprintf("bootnum=%d ", boot_num);
   kprintf("cycles bsort::bsort_main=%ld\n", cycles2 - cycles1);
 
